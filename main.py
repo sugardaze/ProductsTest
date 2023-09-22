00910ca17c5ccd5fa1bae3f158d30738775c12d1
Daria Alles <36181776+sugardaze@users.noreply.github.com>
2023-09-22 17:26:01 +0300

    Без настройки переменных

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import os
-import sys
-os.environ['PYSPARK_PYTHON'] = sys.executable
-os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
-
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("ProductsTest").getOrCreate()
